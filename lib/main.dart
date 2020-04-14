99cd12d1d7c191ece1094b3eeb478a71bea3ed9a
Alan <lanzn16@outlook.com>
2020-04-13 23:26:16 -0300

    add tab navigation

--- lib/main.dart
+++ lib/main.dart
@@ -98,10 +98,56 @@ class _MyHomePageState extends State<MyHomePage> {
                           _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
                           _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
                           _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00'),
+                        ]))),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Container (
+                            height: 65.0,
+                            width: 60.0,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                              borderRadius: BorderRadius.circular(10.0)
+                            ),
+                              child: Center(child: Icon(Icons.search, color: Colors.black),
+                            )
+                          ),
+                          Container (
+                            height: 65.0,
+                            width: 60.0,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                              borderRadius: BorderRadius.circular(10.0)
+                            ),
+                              child: Center(child: Icon(Icons.shopping_basket, color: Colors.black),
+                            )
+                          ),
+                           Container (
+                            height: 65.0,
+                            width: 180.0,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Color(0xFF1C1428)
+                            ),
+                              child: Center(
+                                child: Text('Checkout',
+                                  style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.white,
+                                    fontSize: 15.0))),
+                          ),
                         ],
                       )
-                    ),
-                    )
                 ],
               ),
             )
